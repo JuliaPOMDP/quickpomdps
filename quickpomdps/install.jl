8d01d32072adfb5ba3570e8686e76c4235bb6262
rejuvyesh <mail@rejuvyesh.com>
2019-12-08 22:45:41 -0800

    add registry as well

--- quickpomdps/install.jl
+++ quickpomdps/install.jl
@@ -1,12 +1,13 @@
 using Pkg
 
 Pkg.add("POMDPs")
+POMDPs.add_registry()
 Pkg.add("QuickPOMDPs")
 Pkg.add("QMDP")
 Pkg.add("POMDPSimulators")
 Pkg.add("POMDPPolicies")
 
-using POMDPs
+
 using QuickPOMDPs
 using PyCall
 
