5031b7829c2f989dd472e95f0cb0e4d2ddc889eb
rejuvyesh <mail@rejuvyesh.com>
2021-08-10 03:11:34 +0000

    try getting lightdark example to work

--- examples/lightdark.py
+++ examples/lightdark.py
@@ -1,4 +1,17 @@
 # TODO proper imports (Deterministic from POMDPModelTools, Normal from Distributions
+from quickpomdps import *
+
+from julia.Pkg import add as jlpkg_add
+jlpkg_add("Distributions")
+jlpkg_add("POMDPModelTools")
+
+from julia.Base import Float64
+from julia.POMDPs import solve, pdf
+from julia.Distributions import Normal
+from julia.QMDP import QMDPSolver
+from julia.POMDPSimulators import stepthrough
+from julia.POMDPPolicies import alphavectors
+from julia.POMDPModelTools import Deterministic, Uniform
 
 r = 60
 light_loc = 10
@@ -6,8 +19,8 @@ light_loc = 10
 def transition(s, a):
     if a == 0:
         return Deterministic(r+2)
-    else
-        return Deterministic(min(max(s+a, -r), r)))
+    else:
+        return Deterministic(min(max(s+a, -r), r))
 
 def observation(s, a, sp):
     return Normal(sp, abs(sp - light_loc) + 0.0001)
@@ -27,10 +40,14 @@ m = QuickPOMDP(
     transition = transition,
     observation = observation,
     reward = reward,
-    initialstate = Uniform(range(-r//2, r//2+1))
+    initialstate = Uniform(range(-r//2,r//2+1))
 )
 
 solver = QMDPSolver()
+import pdb; pdb.set_trace()
+## Even this doesn' work
+# from julia.POMDPs import initialstate, reward
+# reward(m, rand(initialstate(m)), 1, 60)
 policy = solve(solver, m)
 
 print('alpha vectors:')
@@ -42,7 +59,7 @@ print()
 rsum = 0.0
 for step in stepthrough(m, policy, max_steps=10):
     print('s:', step.s)
-    print('b:', [pdf(step.b, x) for x in S])
+    print('b:', [pdf(step.b, x) for x in range(-r, r+1)])
     print('a:', step.a)
     print('o:', step.o, '\n')
     rsum += step.r
